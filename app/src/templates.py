f936a45621f753b8d3fdf2035eda4f61026e2ca7
silentmode0n <as.makar@ya.ru>
2025-09-29 22:17:08 +0400

    add templates.py

---
+++ app/src/templates.py
@@ -0,0 +1,37 @@
+INFO_FRAME_FOR_MAF = """Размер:             {width} х {height} мм
+Рама:               {bridge}
+Просвет:            {cliarance} мм
+Фрамуга:            {bridge_height} мм"""
+
+INFO_FRAME_FOR_CL = """Ширина:             {width} мм
+Высота:             {height} мм
+Рама:               {bridge}
+Просвет:            {cliarance} мм
+Фрамуга:            {bridge_height} мм
+Вид заполнения:     {fill}"""
+
+INFO_DOOR = """Размер:             {door_width} х {door_height} мм
+Открытие:           {open}
+Петли (со двора):   {side}"""
+
+INFO_FILL = """Вид заполнения:     {fill}
+Размер на створку:  {door_fill_width} х {door_fill_height} мм
+Размер на фрамугу:  {bridge_fill_width} х {bridge_fill_height} мм"""
+
+INFO_COLOR = """Цвет рамы:          {frame_color} {frame_color_name} {color_type}
+Заполнение снаружи: {fill_color_out} {fill_color_out_name}
+Заполнение изнутри: {fill_color_in} {fill_color_in_name}"""
+
+INFO_OPEN = """Направление:        {open}
+Петли (со двора):   {side}"""
+
+INFO_OPTIONS = """Замок:              {lock}
+Ручка снаружи:      {handle_out}
+Ручка изнутри:      {handle_in}
+Гибкий переход:     {flexible_tube}
+Доводчик:           {auto_closer}
+Нащельник:          {batten} {batten_lenght} мм {batten_num} шт
+В фасаде с воротами:{in_fasad}
+Стикер Аполло:      {sticker}
+Ограничитель RS0301:{bump}
+"""
